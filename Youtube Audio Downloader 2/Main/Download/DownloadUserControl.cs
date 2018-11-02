ecc8463b65f3d7fed672d201e8319928bd63c4ba
Andrea Galvani <17992288+Terminator24@users.noreply.github.com>
2018-11-02 14:26:04 +0100

    Fixed layout and dependencies

--- Youtube Audio Downloader 2/Main/Download/DownloadUserControl.cs
+++ Youtube Audio Downloader 2/Main/Download/DownloadUserControl.cs
@@ -1,17 +1,25 @@
 ﻿using System.Windows.Forms;
+using YoutubeAudioDownloader2.Main.Download.Item;
 
 namespace YoutubeAudioDownloader2.Main.Download
 {
-    public partial class ItemDownloadUserControl : UserControl
+    public partial class DownloadUserControl : UserControl
     {
-        private static ItemDownloadUserControl instance;
-        public static ItemDownloadUserControl Instance { get { if (instance == null) { instance = new ItemDownloadUserControl(); } return instance; } }
+        private static DownloadUserControl instance;
+        public static DownloadUserControl Instance { get { if (instance == null) { instance = new DownloadUserControl(); } return instance; } }
 
-        public ItemDownloadUserControl()
+        public DownloadUserControl()
         {
             InitializeComponent();
 
             Dock = DockStyle.Fill;
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            panel2.Controls.Add(new ItemDownloadUserControl());
+            panel2.Controls.Add(new ItemDownloadUserControl());
+            panel2.Controls.Add(new ItemDownloadUserControl());
         }
     }
 }
