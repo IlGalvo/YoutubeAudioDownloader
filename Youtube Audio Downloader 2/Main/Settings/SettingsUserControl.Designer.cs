10b3c347d4370d37ce23b41c129e8c904033ef15
Andrea Galvani <17992288+Terminator24@users.noreply.github.com>
2018-12-01 02:58:33 +0100

    Fixed SettingsUserControl ratio when resizing.

--- Youtube Audio Downloader 2/Main/Settings/SettingsUserControl.Designer.cs
+++ Youtube Audio Downloader 2/Main/Settings/SettingsUserControl.Designer.cs
@@ -31,24 +31,22 @@
             this.panelActionOffset = new System.Windows.Forms.Panel();
             this.buttonRestore = new System.Windows.Forms.Button();
             this.panelAction = new System.Windows.Forms.Panel();
-            this.panelContent = new System.Windows.Forms.Panel();
             this.tableLayoutPanelSettings = new System.Windows.Forms.TableLayoutPanel();
-            this.groupBoxSearch = new System.Windows.Forms.GroupBox();
-            this.labelSearchResultsStatic = new System.Windows.Forms.Label();
-            this.numericUpDownSearchResults = new System.Windows.Forms.NumericUpDown();
             this.groupBoxGeneral = new System.Windows.Forms.GroupBox();
             this.labelSilentDownloadStatic = new System.Windows.Forms.Label();
-            this.toggleButtonSilentDownload = new YoutubeAudioDownloader.Main.Settings.ToggleButton();
             this.groupBoxDownload = new System.Windows.Forms.GroupBox();
             this.buttonDownloadPath = new System.Windows.Forms.Button();
             this.richTextBoxDownloadPath = new System.Windows.Forms.RichTextBox();
+            this.groupBoxSearch = new System.Windows.Forms.GroupBox();
+            this.labelSearchResultsStatic = new System.Windows.Forms.Label();
+            this.numericUpDownSearchResults = new System.Windows.Forms.NumericUpDown();
+            this.toggleButtonSilentDownload = new YoutubeAudioDownloader.Main.Settings.ToggleButton();
             this.panelActionOffset.SuspendLayout();
-            this.panelContent.SuspendLayout();
             this.tableLayoutPanelSettings.SuspendLayout();
-            this.groupBoxSearch.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSearchResults)).BeginInit();
             this.groupBoxGeneral.SuspendLayout();
             this.groupBoxDownload.SuspendLayout();
+            this.groupBoxSearch.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSearchResults)).BeginInit();
             this.SuspendLayout();
             // 
             // panelActionOffset
@@ -89,23 +87,13 @@
             this.panelAction.Size = new System.Drawing.Size(734, 53);
             this.panelAction.TabIndex = 11;
             // 
-            // panelContent
-            // 
-            this.panelContent.AutoScroll = true;
-            this.panelContent.Controls.Add(this.tableLayoutPanelSettings);
-            this.panelContent.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelContent.Location = new System.Drawing.Point(0, 0);
-            this.panelContent.Name = "panelContent";
-            this.panelContent.Size = new System.Drawing.Size(734, 557);
-            this.panelContent.TabIndex = 2;
-            // 
             // tableLayoutPanelSettings
             // 
             this.tableLayoutPanelSettings.ColumnCount = 1;
             this.tableLayoutPanelSettings.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanelSettings.Controls.Add(this.groupBoxSearch, 0, 0);
             this.tableLayoutPanelSettings.Controls.Add(this.groupBoxGeneral, 0, 2);
             this.tableLayoutPanelSettings.Controls.Add(this.groupBoxDownload, 0, 1);
+            this.tableLayoutPanelSettings.Controls.Add(this.groupBoxSearch, 0, 0);
             this.tableLayoutPanelSettings.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanelSettings.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanelSettings.Name = "tableLayoutPanelSettings";
@@ -116,55 +104,6 @@
             this.tableLayoutPanelSettings.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
             this.tableLayoutPanelSettings.Size = new System.Drawing.Size(734, 557);
             this.tableLayoutPanelSettings.TabIndex = 0;
-            // 
-            // groupBoxSearch
-            // 
-            this.groupBoxSearch.Controls.Add(this.labelSearchResultsStatic);
-            this.groupBoxSearch.Controls.Add(this.numericUpDownSearchResults);
-            this.groupBoxSearch.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBoxSearch.Location = new System.Drawing.Point(9, 9);
-            this.groupBoxSearch.Name = "groupBoxSearch";
-            this.groupBoxSearch.Size = new System.Drawing.Size(716, 175);
-            this.groupBoxSearch.TabIndex = 1;
-            this.groupBoxSearch.TabStop = false;
-            this.groupBoxSearch.Text = "Ricerca";
-            // 
-            // labelSearchResultsStatic
-            // 
-            this.labelSearchResultsStatic.AutoSize = true;
-            this.labelSearchResultsStatic.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelSearchResultsStatic.Location = new System.Drawing.Point(5, 81);
-            this.labelSearchResultsStatic.Name = "labelSearchResultsStatic";
-            this.labelSearchResultsStatic.Size = new System.Drawing.Size(286, 25);
-            this.labelSearchResultsStatic.TabIndex = 2;
-            this.labelSearchResultsStatic.Text = "Numero risultati ricerca (1 - 20):";
-            // 
-            // numericUpDownSearchResults
-            // 
-            this.numericUpDownSearchResults.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.numericUpDownSearchResults.BackColor = System.Drawing.Color.White;
-            this.numericUpDownSearchResults.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.numericUpDownSearchResults.Location = new System.Drawing.Point(660, 82);
-            this.numericUpDownSearchResults.Maximum = new decimal(new int[] {
-            20,
-            0,
-            0,
-            0});
-            this.numericUpDownSearchResults.Minimum = new decimal(new int[] {
-            1,
-            0,
-            0,
-            0});
-            this.numericUpDownSearchResults.Name = "numericUpDownSearchResults";
-            this.numericUpDownSearchResults.ReadOnly = true;
-            this.numericUpDownSearchResults.Size = new System.Drawing.Size(50, 29);
-            this.numericUpDownSearchResults.TabIndex = 3;
-            this.numericUpDownSearchResults.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.numericUpDownSearchResults.Value = new decimal(new int[] {
-            5,
-            0,
-            0,
-            0});
             // 
             // groupBoxGeneral
             // 
@@ -180,6 +119,7 @@
             // 
             // labelSilentDownloadStatic
             // 
+            this.labelSilentDownloadStatic.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.labelSilentDownloadStatic.AutoSize = true;
             this.labelSilentDownloadStatic.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelSilentDownloadStatic.Location = new System.Drawing.Point(5, 81);
@@ -187,19 +127,6 @@
             this.labelSilentDownloadStatic.Size = new System.Drawing.Size(248, 25);
             this.labelSilentDownloadStatic.TabIndex = 8;
             this.labelSilentDownloadStatic.Text = "Attiva download silenzioso:";
-            // 
-            // toggleButtonSilentDownload
-            // 
-            this.toggleButtonSilentDownload.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.toggleButtonSilentDownload.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.toggleButtonSilentDownload.Location = new System.Drawing.Point(660, 81);
-            this.toggleButtonSilentDownload.Name = "toggleButtonSilentDownload";
-            this.toggleButtonSilentDownload.OffColor = System.Drawing.Color.Red;
-            this.toggleButtonSilentDownload.OnColor = System.Drawing.Color.DodgerBlue;
-            this.toggleButtonSilentDownload.Size = new System.Drawing.Size(50, 25);
-            this.toggleButtonSilentDownload.TabIndex = 9;
-            this.toggleButtonSilentDownload.ToggleColor = System.Drawing.Color.White;
-            this.toggleButtonSilentDownload.ToggleState = false;
             // 
             // groupBoxDownload
             // 
@@ -223,7 +150,7 @@
             this.buttonDownloadPath.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonDownloadPath.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonDownloadPath.ForeColor = System.Drawing.Color.DarkGreen;
-            this.buttonDownloadPath.Location = new System.Drawing.Point(660, 81);
+            this.buttonDownloadPath.Location = new System.Drawing.Point(660, 74);
             this.buttonDownloadPath.Name = "buttonDownloadPath";
             this.buttonDownloadPath.Size = new System.Drawing.Size(50, 35);
             this.buttonDownloadPath.TabIndex = 6;
@@ -235,7 +162,7 @@
             this.richTextBoxDownloadPath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.richTextBoxDownloadPath.BackColor = System.Drawing.Color.White;
             this.richTextBoxDownloadPath.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBoxDownloadPath.Location = new System.Drawing.Point(10, 81);
+            this.richTextBoxDownloadPath.Location = new System.Drawing.Point(10, 74);
             this.richTextBoxDownloadPath.Multiline = false;
             this.richTextBoxDownloadPath.Name = "richTextBoxDownloadPath";
             this.richTextBoxDownloadPath.ReadOnly = true;
@@ -244,12 +171,75 @@
             this.richTextBoxDownloadPath.TabIndex = 5;
             this.richTextBoxDownloadPath.Text = "C:\\Users\\Andre\\Music";
             // 
+            // groupBoxSearch
+            // 
+            this.groupBoxSearch.Controls.Add(this.labelSearchResultsStatic);
+            this.groupBoxSearch.Controls.Add(this.numericUpDownSearchResults);
+            this.groupBoxSearch.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBoxSearch.Location = new System.Drawing.Point(9, 9);
+            this.groupBoxSearch.Name = "groupBoxSearch";
+            this.groupBoxSearch.Size = new System.Drawing.Size(716, 175);
+            this.groupBoxSearch.TabIndex = 1;
+            this.groupBoxSearch.TabStop = false;
+            this.groupBoxSearch.Text = "Ricerca";
+            // 
+            // labelSearchResultsStatic
+            // 
+            this.labelSearchResultsStatic.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.labelSearchResultsStatic.AutoSize = true;
+            this.labelSearchResultsStatic.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSearchResultsStatic.Location = new System.Drawing.Point(5, 81);
+            this.labelSearchResultsStatic.Name = "labelSearchResultsStatic";
+            this.labelSearchResultsStatic.Size = new System.Drawing.Size(286, 25);
+            this.labelSearchResultsStatic.TabIndex = 2;
+            this.labelSearchResultsStatic.Text = "Numero risultati ricerca (1 - 20):";
+            // 
+            // numericUpDownSearchResults
+            // 
+            this.numericUpDownSearchResults.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.numericUpDownSearchResults.BackColor = System.Drawing.Color.White;
+            this.numericUpDownSearchResults.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDownSearchResults.Location = new System.Drawing.Point(660, 77);
+            this.numericUpDownSearchResults.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDownSearchResults.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownSearchResults.Name = "numericUpDownSearchResults";
+            this.numericUpDownSearchResults.ReadOnly = true;
+            this.numericUpDownSearchResults.Size = new System.Drawing.Size(50, 29);
+            this.numericUpDownSearchResults.TabIndex = 3;
+            this.numericUpDownSearchResults.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUpDownSearchResults.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // toggleButtonSilentDownload
+            // 
+            this.toggleButtonSilentDownload.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.toggleButtonSilentDownload.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.toggleButtonSilentDownload.Location = new System.Drawing.Point(660, 81);
+            this.toggleButtonSilentDownload.Name = "toggleButtonSilentDownload";
+            this.toggleButtonSilentDownload.OffColor = System.Drawing.Color.Red;
+            this.toggleButtonSilentDownload.OnColor = System.Drawing.Color.DodgerBlue;
+            this.toggleButtonSilentDownload.Size = new System.Drawing.Size(50, 25);
+            this.toggleButtonSilentDownload.TabIndex = 9;
+            this.toggleButtonSilentDownload.ToggleColor = System.Drawing.Color.White;
+            this.toggleButtonSilentDownload.ToggleState = false;
+            // 
             // SettingsUserControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Transparent;
-            this.Controls.Add(this.panelContent);
+            this.Controls.Add(this.tableLayoutPanelSettings);
             this.Controls.Add(this.panelActionOffset);
             this.DoubleBuffered = true;
             this.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -257,21 +247,19 @@
             this.Name = "SettingsUserControl";
             this.Size = new System.Drawing.Size(734, 620);
             this.panelActionOffset.ResumeLayout(false);
-            this.panelContent.ResumeLayout(false);
             this.tableLayoutPanelSettings.ResumeLayout(false);
-            this.groupBoxSearch.ResumeLayout(false);
-            this.groupBoxSearch.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSearchResults)).EndInit();
             this.groupBoxGeneral.ResumeLayout(false);
             this.groupBoxGeneral.PerformLayout();
             this.groupBoxDownload.ResumeLayout(false);
+            this.groupBoxSearch.ResumeLayout(false);
+            this.groupBoxSearch.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSearchResults)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
         private System.Windows.Forms.Panel panelActionOffset;
-        private System.Windows.Forms.Panel panelContent;
         private System.Windows.Forms.Button buttonRestore;
         private System.Windows.Forms.Panel panelAction;
         private System.Windows.Forms.GroupBox groupBoxSearch;
