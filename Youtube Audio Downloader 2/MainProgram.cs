bda641fed8cd3df8cc20069e89174681a23bbf12
Andrea Galvani <17992288+Terminator24@users.noreply.github.com>
2018-12-29 19:31:51 +0100

    Added Updater submodule and logic integration.

--- Youtube Audio Downloader 2/MainProgram.cs
+++ Youtube Audio Downloader 2/MainProgram.cs
@@ -2,6 +2,7 @@
 using System.Reflection;
 using System.Threading;
 using System.Windows.Forms;
+using UpdaterManagerLibrary;
 using YoutubeAudioDownloader2.Main;
 
 namespace YoutubeAudioDownloader2
@@ -17,11 +18,13 @@ namespace YoutubeAudioDownloader2
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            using (Mutex mutex = new Mutex(false, (Application.ProductName + "_" + Assembly.GetExecutingAssembly().GetType().GUID.ToString())))
+            using (Mutex mutex = new Mutex(false, (Application.ProductName + "_" + Assembly.GetExecutingAssembly().GetType().GUID)))
             {
                 if (mutex.WaitOne(0, false))
                 {
-                    //if (UpdateForm.CheckForUpdates(new Version(Application.ProductVersion), false) == DialogResult.OK)
+                    string checkUpdatesUrl = ("https://onedrive.live.com/download?resid=7D7FF9DFDA23C644!1341&authkey=!AAPfdJrVo5UeVkE");
+
+                    if (UpdaterManager.CheckForUpdates(checkUpdatesUrl))
                     {
                         WebBrowserPrepare.SetBrowserFeatureControl();
 
@@ -31,6 +34,7 @@ namespace YoutubeAudioDownloader2
                 else
                 {
                     string text = "Non sono consentite istanze multiple, al momento.";
+
                     MessageBox.Show(text, "Avviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 }
             }
