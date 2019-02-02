8bb16c85aa81e1d3235ad66275f0514bf4996127
Andrea Galvani <17992288+Terminator24@users.noreply.github.com>
2019-02-02 18:51:55 +0100

    Updated nuget packages and update check link.

--- Youtube Audio Downloader/MainProgram.cs
+++ Youtube Audio Downloader/MainProgram.cs
@@ -22,7 +22,7 @@ namespace YoutubeAudioDownloader
             {
                 if (mutex.WaitOne(0, false))
                 {
-                    string updateCheckUrl = ("https://onedrive.live.com/download?resid=7D7FF9DFDA23C644!1341&authkey=!AAPfdJrVo5UeVkE");
+                    string updateCheckUrl = ("https://onedrive.live.com/download?resid=7D7FF9DFDA23C644!2368&authkey=!AFcY04VNisbcdlE");
 
                     if (!UpdateManager.CheckForUpdates(updateCheckUrl))
                     {
