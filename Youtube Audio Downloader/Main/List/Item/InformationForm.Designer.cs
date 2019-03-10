08ec8dab6d616f75d864ba813d73164d1b9a8440
Andrea Galvani <17992288+Terminator24@users.noreply.github.com>
2019-03-10 14:31:20 +0100

    Chenged InformationForm GUI.

--- Youtube Audio Downloader/Main/List/Item/InformationForm.Designer.cs
+++ Youtube Audio Downloader/Main/List/Item/InformationForm.Designer.cs
@@ -53,8 +53,9 @@
             // richTextBoxDescription
             // 
             this.richTextBoxDescription.AutoWordSelection = true;
-            this.richTextBoxDescription.BackColor = System.Drawing.Color.White;
+            this.richTextBoxDescription.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
             this.richTextBoxDescription.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBoxDescription.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.richTextBoxDescription.Location = new System.Drawing.Point(3, 23);
             this.richTextBoxDescription.Name = "richTextBoxDescription";
             this.richTextBoxDescription.ReadOnly = true;
@@ -69,6 +70,7 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBoxDescription.Controls.Add(this.richTextBoxDescription);
+            this.groupBoxDescription.ForeColor = System.Drawing.Color.White;
             this.groupBoxDescription.Location = new System.Drawing.Point(12, 12);
             this.groupBoxDescription.Name = "groupBoxDescription";
             this.groupBoxDescription.Size = new System.Drawing.Size(455, 313);
@@ -81,6 +83,7 @@
             this.groupBoxStatistics.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBoxStatistics.Controls.Add(this.tableLayoutPanelStatistics);
+            this.groupBoxStatistics.ForeColor = System.Drawing.Color.White;
             this.groupBoxStatistics.Location = new System.Drawing.Point(12, 331);
             this.groupBoxStatistics.Name = "groupBoxStatistics";
             this.groupBoxStatistics.Size = new System.Drawing.Size(452, 90);
@@ -121,6 +124,7 @@
             // 
             this.labelViews.AutoSize = true;
             this.labelViews.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelViews.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.labelViews.Location = new System.Drawing.Point(105, 8);
             this.labelViews.Name = "labelViews";
             this.labelViews.Size = new System.Drawing.Size(91, 17);
@@ -131,6 +135,7 @@
             // 
             this.labelOfficial.AutoSize = true;
             this.labelOfficial.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelOfficial.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.labelOfficial.Location = new System.Drawing.Point(105, 31);
             this.labelOfficial.Name = "labelOfficial";
             this.labelOfficial.Size = new System.Drawing.Size(54, 17);
@@ -141,6 +146,7 @@
             // 
             this.labelOfficialStatic.AutoSize = true;
             this.labelOfficialStatic.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelOfficialStatic.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.labelOfficialStatic.Location = new System.Drawing.Point(3, 31);
             this.labelOfficialStatic.Name = "labelOfficialStatic";
             this.labelOfficialStatic.Size = new System.Drawing.Size(57, 17);
@@ -151,6 +157,7 @@
             // 
             this.labelViewsStatic.AutoSize = true;
             this.labelViewsStatic.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelViewsStatic.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.labelViewsStatic.Location = new System.Drawing.Point(3, 8);
             this.labelViewsStatic.Name = "labelViewsStatic";
             this.labelViewsStatic.Size = new System.Drawing.Size(96, 17);
@@ -165,6 +172,7 @@
             this.panelLikesDislikes.Controls.Add(this.labelDislikesStatic);
             this.panelLikesDislikes.Controls.Add(this.labelLikes);
             this.panelLikesDislikes.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelLikesDislikes.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.panelLikesDislikes.Location = new System.Drawing.Point(3, 3);
             this.panelLikesDislikes.Name = "panelLikesDislikes";
             this.panelLikesDislikes.Size = new System.Drawing.Size(217, 58);
@@ -174,6 +182,7 @@
             // 
             this.labelDislikes.AutoSize = true;
             this.labelDislikes.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDislikes.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.labelDislikes.Location = new System.Drawing.Point(100, 31);
             this.labelDislikes.Name = "labelDislikes";
             this.labelDislikes.Size = new System.Drawing.Size(86, 17);
@@ -184,6 +193,7 @@
             // 
             this.labelLikesStatic.AutoSize = true;
             this.labelLikesStatic.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLikesStatic.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.labelLikesStatic.Location = new System.Drawing.Point(3, 8);
             this.labelLikesStatic.Name = "labelLikesStatic";
             this.labelLikesStatic.Size = new System.Drawing.Size(61, 17);
@@ -194,6 +204,7 @@
             // 
             this.labelDislikesStatic.AutoSize = true;
             this.labelDislikesStatic.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDislikesStatic.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.labelDislikesStatic.Location = new System.Drawing.Point(3, 31);
             this.labelDislikesStatic.Name = "labelDislikesStatic";
             this.labelDislikesStatic.Size = new System.Drawing.Size(91, 17);
@@ -204,6 +215,7 @@
             // 
             this.labelLikes.AutoSize = true;
             this.labelLikes.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLikes.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.labelLikes.Location = new System.Drawing.Point(100, 8);
             this.labelLikes.Name = "labelLikes";
             this.labelLikes.Size = new System.Drawing.Size(58, 17);
@@ -215,6 +227,7 @@
             this.linkLabelVideo.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.linkLabelVideo.AutoSize = true;
             this.linkLabelVideo.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabelVideo.LinkColor = System.Drawing.Color.SteelBlue;
             this.linkLabelVideo.Location = new System.Drawing.Point(70, 427);
             this.linkLabelVideo.Name = "linkLabelVideo";
             this.linkLabelVideo.Size = new System.Drawing.Size(81, 20);
@@ -227,7 +240,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.MintCream;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
             this.ClientSize = new System.Drawing.Size(479, 456);
             this.Controls.Add(this.linkLabelVideo);
             this.Controls.Add(this.groupBoxStatistics);
